b8a279ddf50d9a5dfde133db1de31cc036e6d04f
wobey000 <wobey96@gmail.com>
2022-05-18 12:07:36 -0600

    Going to Completely scrap this and start over with smaller increments

--- OpenGLCourseApp.cpp
+++ OpenGLCourseApp.cpp
@@ -4,12 +4,123 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <string.h>
 
 // Window Dimensions
 const GLint WIDTH = 800;
 const GLint HEIGHT = 600; 
 
+GLuint VAO, VBO, shader; 
 
+// Vertex Shader 
+static const char* vShader = "                                  \n\
+#version 330                                                    \n\
+                                                                \n\
+layout (location = 0) in vec3 pos;                              \n\
+                                                                \n\
+void main()                                                     \n\
+{                                                               \n\
+    GL_POSITION = vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);   \n\
+}";
+
+// Fragment Shader 
+static const char* fShader = "                                  \n\
+#version 330                                                    \n\
+                                                                \n\
+out vec4 colour;                                                \n\
+                                                                \n\
+void main()                                                     \n\
+{                                                               \n\
+    colour = vec4(1.0f, 0.0, 0.0, 1.0);                         \n\
+}";
+
+void CreateTriangle()
+{
+    // Create VAO and VBO - define how a triangle should be drawn - multiple VBOs to a VAO
+    GLfloat vertices[] = {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f
+    };
+
+    glGenVertexArrays(1, &VAO); 
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1, &VBO); 
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); 
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); 
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0); 
+}
+
+void AddShaders(GLuint theProgram, const char* shaderCode, GLenum shaderType)
+{
+    GLuint theShader = glCreateShader(shaderType); // pass shader type
+
+    const GLchar* theCode[1]; 
+    theCode[0] = shaderCode;
+
+    GLint codeLength[1];
+    codeLength[0] = strlen(shaderCode); 
+
+    glShaderSource(theShader, 1, theCode, codeLength);
+    glCompileShader(theShader); 
+
+    GLint result = 0;
+    GLchar eLog[1024] = { 0 };
+
+    glGetProgramiv(theShader, GL_COMPILE_STATUS, &result);
+    if (!result)
+    {
+        glGetShaderInfoLog(theShader, sizeof(eLog), NULL, eLog);
+        printf("Error comiling the  %d shader: '%s' \n", shaderType, eLog);
+        return;
+    }
+
+    glGetAttachedShaders()
+
+    return; 
+}
+
+void CompileShaders()
+{
+    shader = glCreateProgram(); 
+
+    if (!shader)
+    {
+        printf("Error creating shader program! \n");
+        return; 
+    }
+    AddShaders(shader, vShader, GL_VERTEX_SHADER);
+    AddShaders(shader, fShader, GL_FRAGMENT_SHADER);
+
+    GLint result = 0; 
+    GLchar eLog[1024] = { 0 }; 
+
+    glLinkProgram(shader); 
+    glGetProgramiv(shader, GL_LINK_STATUS, &result); 
+    if (!result)
+    {
+        glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+        printf("Error linking program: '%s' \n", eLog); 
+        return;
+    }
+
+    glValidateProgram(shader); 
+    glGetProgramiv(shader, GL_VALIDATE_STATUS, &result);
+    if (!result)
+    {
+        glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+        printf("Error validating program: '%s' \n", eLog);
+        return;
+    }
+
+}
 
 int main()
 {
@@ -59,8 +170,11 @@ int main()
         return 1; 
     }
 
-    // Setup Viewport Size 
+    // Create Viewport
     glViewport(0, 0, bufferWidth, bufferHeight); 
+
+    CreateTriangle();
+    CompileShaders(); 
 
     // Loop until window closed 
     while (!glfwWindowShouldClose(mainWindow))
@@ -69,8 +183,17 @@ int main()
         glfwPollEvents(); 
 
         // clear window
-        glClearColor(1.0f, 0.0f, 0.0f, 1.0f); // clear entire screen and set color - RGB Value 0.0-1.0)
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // clear entire screen and set color - RGB Value 0.0-1.0)
         glClear(GL_COLOR_BUFFER_BIT); 
+
+        //actual drawing 
+        glUseProgram(shader);
+
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
+        glUseProgram(0); 
 
         glfwSwapBuffers(mainWindow);
     }
